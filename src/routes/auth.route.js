8d3e4a51fad54ebfe6cc2b8f842a300fd17b0fe8
Swaraj0769 <swarajsanodiya6@gmail.com>
2025-08-01 21:53:37 +0530

    Connect loginController

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,6 +2,7 @@ const express = require('express')
 const userModel = require('../models/user.model')
 const jwt = require('jsonwebtoken')
 const {registerController} = require('../controllers/auth.controller')
+const {loginController} = require('../controllers/auth.controller')
 const { mongo, default: mongoose } = require('mongoose')
 
 const router = express.Router()
@@ -13,6 +14,8 @@ Get /user [protected]
 */
 
 router.post('/register', registerController)
+
+router.post('/login', loginController)
 
 
 module.exports = router
