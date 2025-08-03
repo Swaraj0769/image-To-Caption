98ae9f6351071ed5f05c4eb0bd5337dff2628c3f
Swaraj0769 <swarajsanodiya6@gmail.com>
2025-08-03 09:49:54 +0530

    attach middleware in between api and createPostController

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,31 +1,12 @@
 const express = require('express')
-const jwt = require('jsonwebtoken')
 const router = express.Router()
-const userModel = require('../models/user.model')
+const multer = require('multer')
+const authMiddleware = require('../middlewares/auth.middleware')
+const createPostController = require('../controllers/post.controller')
+
+
 
 /* POST /api/post [protected] */
-router.post('/',async (req, res)=>{
-    const token = req.cookies.token;
-
-    if(!token){
-        return res.status(401).json({
-            message:"Unauthorized access, please login first"
-        })
-    }
-
-    try{
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-        const user = await userModel.findOne({
-            _id: decoded.id 
-        })
-
-        // req.user = user
-    }catch(error){
-        return res.status(401).json({
-            message:"Invalid token, please login again"
-        })
-    }
-})
+router.post('/',authMiddleware,upload.single("image"), createPostController )
 
 module.exports = router;
