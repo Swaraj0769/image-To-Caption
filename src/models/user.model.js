da301dd24fc1200ec2b241569e5d730a10344eb3
Swaraj0769 <swarajsanodiya6@gmail.com>
2025-08-03 13:09:36 +0530

    connect to another DB folder

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -12,6 +12,6 @@ const userSchema = new mongoose.Schema({
     },
 })
 
-const userModel = mongoose.model("user", userSchema)
+const userModel = mongoose.model("AuthWithJWT", userSchema)
 
 module.exports = userModel
